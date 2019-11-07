69027492bcda99bbed16b2f16a95853ee9a0747f
Aleksandr Bondarev <a.bondarev@zmeke.com>
2019-11-07 13:32:33 +0300

    counters

--- src/DumpCodeCache.java
+++ src/DumpCodeCache.java
@@ -4,6 +4,7 @@ import sun.jvm.hotspot.code.CodeBlob;
 import sun.jvm.hotspot.code.CodeCacheVisitor;
 import sun.jvm.hotspot.code.NMethod;
 import sun.jvm.hotspot.debugger.Address;
+import sun.jvm.hotspot.oops.Method;
 import sun.jvm.hotspot.runtime.VM;
 import sun.jvm.hotspot.tools.Tool;
 
@@ -25,10 +26,21 @@ public class DumpCodeCache extends Tool {
                 if (blob != null) {
                     if (blob instanceof NMethod) {
                         NMethod method = (NMethod) blob;
-                        if (method.getMethod() == null) {
+                        Method internalMethod = method.getMethod();
+                        if (internalMethod == null) {
                             System.out.printf("%8d\tnull method\n", method.getSize());
                         } else {
-                            System.out.printf("%8d\t%s\n", method.getSize(), method.getName());
+                            long invocationCount = 0L;
+                            long backedgeCount = 0L;
+                            int interpreterInvocationCount = 0;
+                            int interpreterThrowoutCount = 0;
+                            if (internalMethod.getMethodCounters() != null) {
+                                invocationCount = internalMethod.getInvocationCount();
+                                backedgeCount = internalMethod.getBackedgeCount();
+                                interpreterInvocationCount = internalMethod.interpreterInvocationCount();
+                                interpreterThrowoutCount = internalMethod.interpreterThrowoutCount();
+                            }
+                            System.out.printf("%8d\t%s\t%s\t%s\t%s\t%s\t%s\n", method.getSize(), method.getName(), invocationCount, backedgeCount, interpreterInvocationCount, interpreterThrowoutCount, method.isOSRMethod());
                         }
                     } else {
                         System.out.printf("%8d\t%s\n", blob.getSize(), Objects.toString(blob.getName(), "null"));
